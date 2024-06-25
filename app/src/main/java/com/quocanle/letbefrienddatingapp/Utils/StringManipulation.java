a7c4ae7e664fc98cb8973fc0faeaa24d144a900e
VinhPham131 <vinh.pham220212@vnuk.edu.vn>
2024-06-25 10:59:09 +0700

    add Utils

---
+++ app/src/main/java/com/quocanle/letbefrienddatingapp/Utils/StringManipulation.java
@@ -0,0 +1,12 @@
+package com.quocanle.letbefrienddatingapp.Utils;
+
+public class StringManipulation {
+
+    public static String expandUsername(String username) {
+        return username.replace(".", " ");
+    }
+
+    public static String condenseUsername(String username) {
+        return username.replace(" ", ".");
+    }
+}
